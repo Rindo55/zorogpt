032247c6721b31815cde0674c874740ee87b6d00
Arpan Rathore <119804213+foreverlivin@users.noreply.github.com>
2023-01-22 19:16:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ import pytz
 import telegram
 import logging
 import dotenv
+from pyrogram import filters, Client
 from modules.chat import Chat
 from modules.google import Google
 from modules.memory import clear_history

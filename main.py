a6b72959362da88b9a3364265f43a19b93cd55f0
Arpan Rathore <119804213+foreverlivin@users.noreply.github.com>
2023-01-22 19:55:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -164,7 +164,7 @@ async def error(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 
 def main():
     # Handle messages
-    application.add_handler(filters.Regex("@zorolostbot", send))
+    application.add_handler(filters.TEXT & filters.Entity('@zorolostbot', send))
     application.add_handler(CommandHandler("bye", start))
     application.add_handler(CommandHandler("reset", reset))
     application.add_handler(CommandHandler("noschedule", schedule))

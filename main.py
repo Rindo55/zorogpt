83c0e99106f1cf136c03f30532e52887ede545ab
Arpan Rathore <119804213+foreverlivin@users.noreply.github.com>
2023-01-22 18:55:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -52,8 +52,7 @@ google = Google(os.getenv("SERP_API_KEY"))
 # set up the scheduler
 scheduler = Scheduler(application.job_queue)
 
-@application.on_message(filters.command(["chat", f"chat@zorolostbot"], prefixes=trg))
-@auth()
+@auth.on_message(filters.command(["chat", f"chat@zorolostbot"], prefixes=trg))
 async def send(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Send message to OpenAI"""
     chat = get_chat(update, context)
